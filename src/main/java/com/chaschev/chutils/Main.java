3594bffbdc53615b50e8c27135b51ac00f311e30
chaschev <chaschev@gmail.com>
2013-10-02 11:26:30 +0400

    Updated dialog for the Installation example.

--- src/main/java/com/chaschev/chutils/Main.java
+++ src/main/java/com/chaschev/chutils/Main.java
@@ -7,11 +7,17 @@ import com.google.common.base.Strings;
  */
 public class Main {
     public static void main(String[] args) {
+        if(args.length != 2){
+            System.out.println("USAGE: chutils <your name> <a word>");
+        }
+
         System.out.printf("hi from chutils, %s! have a good %s!%n", args[0], args[1]);
-        System.out.println("is guava with us?");
+        System.out.printf("are we alone, %s?%n%n", args[0]);
 
-        System.out.print("guava is");
+        System.out.print("no, we celebrate! guava is");
 
-        System.out.println(Strings.commonSuffix(" no, not here!", " here!") );
+        System.out.flush();
+
+        System.out.println(Strings.commonSuffix(" not with us!", " with us!") );
     }
 }
